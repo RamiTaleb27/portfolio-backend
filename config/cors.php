30d4a8d071b3f5364128632fdec403563fbdd83a
unknown <rami.m.taleb@gmail.com>
2026-08-15 11:45:53 +0300

    Update CORS for Vercel frontend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+   'allowed_origins' => [
+    'http://localhost:5173',
+    '],https://portfolio-frontend-one-snowy-25.vercel.app/',
+],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
